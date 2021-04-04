34c7a5d698a9b6a3448ffbfd62a5f0d0c5ef0c31
larryw3i <larryw3i@163.com>
2021-04-04 19:54:04 +0800

    Add menubar

--- openitcr/ui/all_you_think.py
+++ openitcr/ui/all_you_think.py
@@ -6,35 +6,38 @@ import tkinter.ttk as ttk
 class MainFrame:
     def __init__(self, master=None):
         # build ui
-        if master == None:
-            _main = tk.Tk()
+        
+        self.master = tk.Tk() if master == None else master
 
-            menubar = Menu(_main)
-            filemenu = Menu(menubar)
-            filemenu.add_command(label="Exit" )
-            menubar.add_cascade(label="File", menu=filemenu)
-            
-            _main.config( menu = menubar )
-
-        self._mainf = tk.Frame(_main)
+        self._mainf = tk.Frame( self.master )
         self.mainf = ttk.Frame(self._mainf)
 
-        self.alldesktopslf = ttk.Labelframe(self.mainf)
-        self.alldsb = tk.Scrollbar(self.alldesktopslf)
+        self.desktopslf = ttk.Labelframe(self.mainf)
+        self.alldsb = tk.Scrollbar(self.desktopslf)
         self.alldsb.configure(orient='vertical')
         self.alldsb.pack(side='right')
-        self.alldesktopslf.configure(height='200', text='All desktops', width='450')
-        self.alldesktopslf.pack(side='top')
+        self.desktopslf.configure(height='200', text='All desktops', width='450')
+        self.desktopslf.pack(side='top')
         self.mainf.pack(side='top')
         self.langcbb = ttk.Combobox(self._mainf)
         self.langcbb.configure(justify='right')
         self.langcbb.pack(anchor='e', side='bottom')
         self._mainf.configure(height='250', width='600')
         self._mainf.pack(side='top')
+        self.config_menubar()
 
         # Main widget
         self.mainwindow = self._mainf
 
+    def config_menubar( self ):
+        menubar = Menu(self.master)
+
+        filemenu = Menu(menubar)
+        filemenu.add_command(label="Exit" )
+        
+        menubar.add_cascade(label="File", menu=filemenu)
+        
+        self.master.config( menu = menubar )
 
     def run(self):
         self.mainwindow.mainloop()
