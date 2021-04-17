d1109d46b965d379ae74c7cdeb46f5f45e627ff9
larryw3i <larryw3i@163.com>
2021-04-17 19:24:07 +0800

    zZ

--- openitcr.sh
+++ openitcr.sh
@@ -3,7 +3,6 @@
 git_push(){
     echo 'Push to git@gitee.com:larryw3i/openitcr.git'
     git push git@gitee.com:larryw3i/openitcr.git
-
     echo 'Push to git@github.com:larryw3i/openitcr.git'
     git push git@github.com:larryw3i/openitcr.git
 }
